c89870c7ea11b61ead2289a2003ebd55033352a7
Arunarivalagan743 <arunarivalagan774@gmail.com>
2026-06-14 18:08:23 +0530

    java connects the mysql

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,43 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.*;
+import java.util.Properties;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Properties props = new Properties();
+
+        try (FileInputStream fis = new FileInputStream("db.properties")) {
+            props.load(fis);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        String url = props.getProperty("db.url");
+        String user = props.getProperty("db.user");
+        String password = props.getProperty("db.password");
+
+        String sql =
+                "SELECT birth_date FROM customers WHERE customer_id = ? AND first_name = ?";
+
+        try (
+                Connection con =
+                        DriverManager.getConnection(url, user, password);
+                PreparedStatement ps =
+                        con.prepareStatement(sql)
+        ) {
+            ps.setInt(1,7);
+            ps.setString(2,"Ilene");
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                System.out.println(
+                        rs.getDate("birth_date")
+                );
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 }
